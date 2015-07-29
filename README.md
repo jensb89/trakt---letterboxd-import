44551c7ed79d6f85d2ea68410399c0d734f694df
Jens Brauer <ensb89@users.noreply.github.com>
2015-07-29 17:59:46 +0200

    Trakt API v2 compatibility, solves #1
    Now the script can be used with the new trakt api v2 :)

--- README.md
+++ README.md
@@ -5,10 +5,9 @@ Import Letterboxd movie list (diary) into trakt.tv
 
 ##Usage:##
 1. Export your Letterboxd data (under Settings->Import&Export)
-2. $ python py-trakt-letterboxd-import.py diary.csv
-
-In addition your trakt.tv username, password and apikey have either to be filled in into the python file or call py-trakt-letterboxd-import with --username yourname --password pwd --apikey key
-
+2. Create a trakt.tv application (http://trakt.tv/oauth/applications). Use "urn:ietf:wg:oauth:2.0:oob" as the redirect URI.
+3. Fill in the client id, client secret and the code in the python file. The code is obtained by pointing your browser to the Pin-URL.
+4. $ python py-trakt-letterboxd-import.py diary.csv
 
 ##Thanks##
 Some code-parts were taken from https://github.com/akampjes/trakt-list-import !
--- py-trakt-letterboxd-import.py
+++ py-trakt-letterboxd-import.py
@@ -9,12 +9,92 @@ import time
 import hashlib
 import getpass
 import csv
+import time
+import os.path
 
 
-USERNAME = ''
-PASSWORD = ''
-APIKEY = ''
-PASSWORD = hashlib.sha1(PASSWORD).hexdigest()
+CLIEN_ID= ''
+CLIEN_SECRECT = ''
+CODE = ''
+
+
+def check_authentication():
+    if os.path.isfile('auth.json'):
+        with open('auth.json') as auth_file:
+            auth_data = json.load(auth_file)
+            TOKEN = auth_data['access_token']
+            REFRESH_TOKEN = auth_data['refresh_token']
+            expired = (time.time()-auth_data['created_at']) >=  auth_data['expires_in']
+
+            if expired:
+                print 'Authentification expired, trying to re-authentificate...'
+                authorize(REFRESH_TOKEN,'refresh')
+                return check_authentication()
+
+            return TOKEN
+
+    else:
+        print 'Authentication File not found!\n Trying first authentication ...' 
+        authorize(CODE)
+        return check_authentication()
+
+
+def authorize(auth_code, grant_type='authorization_code', code=CODE):
+
+    if grant_type == 'authorization_code':
+
+        values = {
+            "code": code,
+            "client_id": CLIEN_ID,
+            "client_secret": CLIEN_SECRECT,
+            "redirect_uri": "urn:ietf:wg:oauth:2.0:oob",
+            "grant_type": "authorization_code"
+        }
+
+    elif grant_type == 'refresh_token':
+
+        values = {
+            "refresh_token": code,
+            "client_id": CLIEN_ID,
+            "client_secret": CLIEN_SECRECT,
+            "redirect_uri": "urn:ietf:wg:oauth:2.0:oob",
+            "grant_type": "refresh_token"
+        }
+
+    else:
+        print 'grant_type has to be authorization_code or refresh_token!!!'
+
+    headers = {
+      'Content-Type': 'application/json'
+    }
+    request = urllib2.Request('https://api-v2launch.trakt.tv/oauth/token', data=json.dumps(values), headers=headers)
+
+    try:
+        r = urllib2.urlopen(request)
+    except urllib2.HTTPError as e:
+        if e.code == 401:
+            print 'CODE maybe wrong / already used?'
+            raise Exception("OAuth must be provided")
+        elif e.code == 403:
+            raise Exception("Invalid API key")
+        elif e.code == 400:
+            raise Exception("Bad Request - request couldn't be parsed")
+        elif e.code == 429:
+            raise Exception("Rate Limit Exceeded")
+        elif e.code == 500 or e.code == 503 or e.code==520 or e.code==521 or e.code==522:
+            raise Exception("Server Error")
+    else:
+        # 200
+        print 'Authentication sucessfull.'
+        req = r.read()
+        #print req
+
+        print 'Saving authentication token ...'
+        f = open('auth.json','w')
+        print >> f, req
+        f.close()
+
+    #print response_body
 
 
 def get_data_letterboxd_diary(filename):
@@ -33,24 +113,27 @@ def get_data_letterboxd_diary(filename):
 
             imdbid = imdbinfo.get('imdbID',None)
             data.append([row[1],row[2],row[6]+' 20:15',imdbid])
+            print [row[1],row[2],row[6]+' 20:15',imdbid]
             time.sleep(0.2)
 
     return data
 
-def send_data(movie_data):
+def send_data(movie_data, auth_token):
     #movie_data = [{'imdb_id':imdb_id,'title':title,'year':year,'last_played': date_played}]
     pydata = {
-            'username':USERNAME,
-            'password':PASSWORD,
             'movies': movie_data
             }
     json_data = json.dumps(pydata)
     clen = len(json_data)
-    #print json_data
-    #f = open('sample.json', 'w')
-    #print >> f, json_data
-    #f.close()
-    req = urllib2.Request("https://api.trakt.tv/movie/seen/"+APIKEY, json_data, {'Content-Type': 'application/json', 'Content-Length': clen})
+
+    headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + auth_token,
+    'trakt-api-version': '2',
+    'trakt-api-key': CLIEN_ID
+    }
+    req = urllib2.Request('https://api-v2launch.trakt.tv/sync/history', data=json_data, headers=headers)
+
     f = urllib2.urlopen(req)
     response = f.read()
     f.close()
@@ -71,15 +154,10 @@ def get_imdb_info(title, year=None):
 def usage(argv0):
     print('usage: '+argv0+' [OPTION] FILE ...')
     print('''
-
     OPTIONS
     -h
     --help          Show help
-    --username=     Your username
-    --password=     Your password (not recommended, instead enter password when prompted)
-    --apikey=       Your API key (get from here: http://trakt.tv/api-docs/authentication)
     --debug         Turn on debuging mode (doesn't exist)
-
     EXAMPLE USAGE
         python py-trakt-letterboxd-import.py diary.csv
     ''')
@@ -88,7 +166,7 @@ def usage(argv0):
 if __name__ == "__main__":
     try:
         args, letterboxd_file = getopt.gnu_getopt(sys.argv[1:], 'h',[
-            'help','username=','password=','apikey='])
+            'help'])
     except:
         print "\nPlease  c h e c k   a r g u m e n t s\n\n"
         usage(sys.argv[0])
@@ -96,22 +174,12 @@ if __name__ == "__main__":
     for opt, arg in args:
         if opt in ['--help', '-h']:
             usage(sys.argv[0])
-        elif opt in ['--username']:
-            USERNAME = arg
-        elif opt in ['--password']:
-            PASSWORD = hashlib.sha1(arg).hexdigest()
-        elif opt in ['--apikey']:
-            APIKEY = arg
         else:
             print('unknown option '+opt)
             usage(sys.argv[0])
 
-    if USERNAME == '':
-        USERNAME = raw_input("enter trakt username: ")
-    if APIKEY == '':
-        APIKEY = raw_input("enter trakt apikey: ")
-    if PASSWORD == '':
-        PASSWORD = hashlib.sha1(getpass.getpass("enter trakt password (input hidden):")).hexdigest()
+    print 'Authentication with Trakt.tv....'
+    token = check_authentication()
 
     print letterboxd_file[0]
     data = get_data_letterboxd_diary(letterboxd_file[0])
@@ -123,13 +191,15 @@ if __name__ == "__main__":
         year = line[1]
         date_played = line[2]
         imdbid = line[3]
-        movie_data.append({'imdb_id':imdbid,'title':title,'year':int(year),'last_played': int(time.mktime(time.strptime(date_played, '%Y-%m-%d %H:%M')))})
+        #print date_played
+        #print time.strftime(date_played, '%Y-%m-%d %H:%M')
+        movie_data.append({'title':title,'year':int(year),'watched_at': date_played,'ids':{'imdb_id':imdbid}})
 
         # send batch of 100 IMDB IDs
         if len(movie_data) >= 100:
             print 'Importing first 100 movies...'
-            send_data(movie_data)
+            send_data(movie_data,auth_token=token)
             movie_data = []
 
     if len(movie_data) > 0:
-            send_data(movie_data)
+            send_data(movie_data,auth_token=token)
